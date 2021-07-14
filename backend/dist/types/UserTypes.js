3c1a856d98d65ab12fa019527077c0e72702ed42
gadfaria <gadfaria@gmail.com>
2021-07-13 22:30:27 -0300

    fix: Deleting dist

--- backend/dist/types/UserTypes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-/**
- * For constructing schemas, check:
- * @see https://github.com/YousefED/typescript-json-schema/blob/master/api.md
- */
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=UserTypes.js.map
